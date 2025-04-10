a20b6dc9624199d3e22f160ad05daa69af115555
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2025-04-10 08:57:52 +0200

    update obsidian version

--- obsidian_install.sh
+++ obsidian_install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.3/obsidian_1.5.3_amd64.deb
-sudo apt-get -y install ./obsidian_1.5.3_amd64.deb
-rm -rf obsidian_1.5.3_amd64.deb
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.9/obsidian_1.8.9_amd64.deb
+sudo apt-get -y install ./obsidian_1.8.9_amd64.deb
+rm -rf obsidian_1.8.9_amd64.deb
 
