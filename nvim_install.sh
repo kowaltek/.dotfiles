1a283315025a8530f0d517e36e8140dc2a2b306a
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2025-05-12 09:00:01 +0200

    nvim install script update

--- nvim_install.sh
+++ nvim_install.sh
@@ -2,17 +2,7 @@
 
 set -e
 
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage.sha256sum
-sha256sum nvim.appimage
-
-if [[ "$(sha256sum -c nvim.appimage.sha256sum)" != "nvim.appimage: OK" ]];
-then
-    rm -rf nvim.appimage*
-    printf "\n\n    Checksum for neovim not matching - exiting!\n\n"
-    exit 1
-fi
-
-mv nvim.appimage ~/Applications/nvim
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
+mv nvim-linux-x86_64.appimage ~/Applications/nvim
 chmod +x ~/Applications/nvim
-rm -rf nvim.appimage*
+rm -rf nvim-linux-x86_64.appimage*
