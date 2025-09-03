290b0c5cf4d88f8615a57aba594461caa97e29b1
Mateusz Kowalke <mateusz.kowalke@gmail.com>
2025-09-03 13:17:30 +0200

    update lsp configs to newer version

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -6,14 +6,4 @@ return {
 			require("mason").setup()
 		end,
 	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			-- need to install "sql-formatter" manually through mason as it's formatter
-			require("mason-lspconfig").setup({
-				automatic_installation = true,
-			})
-		end,
-	},
 }
